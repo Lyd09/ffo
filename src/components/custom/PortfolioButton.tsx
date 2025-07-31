46ac87a613fa3ac98a867d2bf7fdd7d4128a9376
Shu Otaku <senhordosubmundo0@gmail.com>
2025-07-31 14:29:19 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <button class="por

--- src/components/custom/PortfolioButton.tsx
+++ src/components/custom/PortfolioButton.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Link from 'next/link';
 
 const PortfolioButton = ({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
     <button className="portfolio-btn" {...props}>
-      {children}
+        <span className="flex items-center justify-center">
+            {children}
+        </span>
     </button>
   );
 };
