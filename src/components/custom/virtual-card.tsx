3ed437d222edba9916de3d87ed0c7f40e7d7870a
Shu Otaku <senhordosubmundo0@gmail.com>
2025-09-07 06:10:24 +0000

    hm...de fato, resolveu os problemas. O ícone do calendário está sendo co

--- src/components/custom/virtual-card.tsx
+++ src/components/custom/virtual-card.tsx
@@ -136,9 +136,7 @@ export default function VirtualCard() {
               <button className="sparkle-button">
                 <span className="spark"></span>
                 <span className="backdrop"></span>
-                <svg className="sparkle" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <Calendar />
-                </svg>
+                <Calendar className="sparkle" />
                 <span className="text">Marque uma reunião!</span>
               </button>
               <div className="bodydrop"></div>
