a60e2b786f659d614c4b09e700854aaa8c0d803b
Shu Otaku <senhordosubmundo0@gmail.com>
2025-09-07 06:51:03 +0000

    Esse novo grupo chamado contato não ta legal, cara. Pelo menos, troque c

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -160,7 +160,7 @@ export default function MainLayout({
                 </div>
               </div>
               <div className="flex flex-col items-center">
-                <h4 className="font-semibold text-sm uppercase tracking-wider text-muted-foreground mb-3">Contato</h4>
+                <h4 className="font-semibold text-sm uppercase tracking-wider text-muted-foreground mb-3">Cartão</h4>
                 <Link href="/cartao" passHref>
                   <Button variant="ghost" className="hover:bg-primary/10 hover:text-primary px-2">
                     <Contact className="mr-2 h-5 w-5" />
