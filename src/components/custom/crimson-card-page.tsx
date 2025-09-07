a0ff85f76bd96e46ca168ab35a52368199a33308
Shu Otaku <senhordosubmundo0@gmail.com>
2025-09-07 05:44:37 +0000

    meus botões estão bugados ness pagina, por que?

--- src/components/custom/crimson-card-page.tsx
+++ src/components/custom/crimson-card-page.tsx
@@ -4,10 +4,8 @@ import VirtualCard from '@/components/custom/virtual-card';
 
 export default function CrimsonCardPage() {
   return (
-    <div className="w-full max-w-6xl mx-auto">
-      <div className="flex items-center justify-center">
-        <VirtualCard />
-      </div>
+    <div className="w-full max-w-lg mx-auto">
+      <VirtualCard />
     </div>
   );
 }
