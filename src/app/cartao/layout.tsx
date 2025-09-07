0414c66826ebb55b6f5043358f2f34eb3f842d3f
Shu Otaku <senhordosubmundo0@gmail.com>
2025-09-07 05:55:08 +0000

    Try fixing this error: `Console Error: In HTML, <html> cannot be a child

--- src/app/cartao/layout.tsx
+++ src/app/cartao/layout.tsx
@@ -2,7 +2,6 @@
 import '../virtual-card-social.css';
 import '../virtual-card-contact.css';
 import '../virtual-card-meeting.css';
-import '../globals.css';
 
 export const metadata = {
   title: 'Cartão de Visita - FastFilms',
@@ -15,19 +14,8 @@ export default function CartaoLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="dark">
-        <head>
-            <link rel="icon" href="/index/LOGO-ICON.svg" type="image/svg+xml" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-            <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
-            rel="stylesheet"
-            />
-        </head>
-        <body className="font-body antialiased bg-gradient-to-r from-[#121212] via-[#3a2f2f] to-[#121212] flex items-center justify-center min-h-screen p-4">
-            {children}
-        </body>
-    </html>
+    <div className="font-body antialiased bg-gradient-to-r from-[#121212] via-[#3a2f2f] to-[#121212] flex items-center justify-center min-h-screen p-4">
+        {children}
+    </div>
   );
 }
